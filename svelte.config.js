6e573b2a4da58d0402d584568d10daf28a949c59
Viggo Petersen <vig56@hotmail.com>
2023-01-19 23:21:51 +0100

    add prerender fallback

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,10 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			strict: false,
+			pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+			precompress: false,
 		}),
 	},
 };
